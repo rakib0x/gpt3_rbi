81fef93a44d2e40050eba65c5f657861a7fbc4b4
Rakib <rakib02.08.23@gmail.com>
2024-05-04 14:06:36 +0600

    Adding Header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Navbar } from "./components";
 
 import "./App.css";
+import { Header } from "./containers";
 
 function App() {
   return (
     <>
       <div className="gradient__bg">
         <Navbar />
+        <Header />
       </div>
     </>
   );
